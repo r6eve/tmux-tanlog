7cda3f658f0bf5663d5e11fc53235598760c236d
r6eve <r6eve179f@gmail.com>
2019-08-17 14:34:14 +0900

    Add deploy workflow

---
+++ ci/before_deploy.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -eu
+
+build() {
+  cargo build --target "$TARGET" --release --verbose
+}
+
+pack() {
+  local -r tempdir=$(mktemp -d 2>/dev/null || mktemp -d -t tmp)
+  local -r package_name="$PROJECT_NAME-$TRAVIS_TAG-$TARGET"
+  mkdir "$tempdir/$package_name"
+  cp "target/$TARGET/release/$PROJECT_NAME" README.md LICENSE_1_0.txt \
+    "$tempdir/$package_name"
+  pushd "$tempdir"
+  tar cJf "$(pwd)/$package_name.tar.xz" "$package_name"
+  popd
+  rm -rf "$tempdir"
+}
+
+main() {
+  build
+  pack
+}
+
+main
