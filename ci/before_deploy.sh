9a612c822399ef41c0d78b9c2655db8c2b5fd0a8
r6eve <r6eve179f@gmail.com>
2019-08-18 12:04:09 +0900

    Fix working directory

--- ci/before_deploy.sh
+++ ci/before_deploy.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
-set -eu
+set -eux
 
 build() {
   cargo build --target "$TARGET" --release --verbose
 }
 
 pack() {
+  local -r pwd=$(pwd)
   local -r tempdir=$(mktemp -d 2>/dev/null || mktemp -d -t tmp)
   local -r package_name="$PROJECT_NAME-$TRAVIS_TAG-$TARGET"
   mkdir "$tempdir/$package_name"
   cp "target/$TARGET/release/$PROJECT_NAME" README.md LICENSE_1_0.txt \
     "$tempdir/$package_name"
   pushd "$tempdir"
-  tar cJf "$(pwd)/$package_name.tar.xz" "$package_name"
+  tar cJf "$pwd/$package_name.tar.xz" "$package_name"
   popd
   rm -rf "$tempdir"
 }
